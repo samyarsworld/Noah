1602cb96f63b363e3db8a73544c64edb3bf8cfdd
samyarsworld <samyarfarjam@outlook.com>
2023-03-27 15:12:42 -0400

    Final development on local server

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,26 +1,14 @@
 const mongoose = require("mongoose");
 
-// const databaseConnect = () => {
-//   mongoose.set('strictQuery', true);
-//   mongoose.connect(process.env.MONGODB_ATLAS_URL)
-//     .then(() => console.log('Connected to mongo'))
-//     .catch((err) => {
-//       console.error('Failed to connect with mongo');
-//       console.error(err);
-//     });
-// };
-
 const databaseConnect = () => {
+  mongoose.set("strictQuery", true);
   mongoose
-    .connect(process.env.DATABASE_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("Mongodb Database Connected");
-    })
-    .catch((error) => {
-      console.log(error);
+    .connect(process.env.MONGODB_ATLAS_URL)
+    .then(() => console.log("Connected to mongo"))
+    .catch((err) => {
+      console.error("Failed to connect with mongo");
+      console.error(err);
     });
 };
+
 module.exports = databaseConnect;
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const http = require("http");
+const socketServer = require("./socket");
 
 const databaseConnect = require("./config/database");
 const authRouter = require("./routes/authRoute");
@@ -13,18 +15,20 @@ dotenv.config({
   path: "backend/config/config.env",
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
+// app.use(cors());
 app.use(bodyParser.json({ limit: "10mb" }));
-// app.use(bodyParser.json());
-
 app.use(cookieParser());
 
 app.use("/api/chat", authRouter);
 app.use("/api/chat", chatRouter);
 
-databaseConnect();
+const server = http.createServer(app);
 
-app.listen(port, () => {
+databaseConnect();
+socketServer(server);
+
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:5000/`);
 });
