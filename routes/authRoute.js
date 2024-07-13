ae3b88e72e1db999562e13f5bda632f6c74bac74
Samyar Farjam <samyarfarjam@outlook.com>
2024-07-13 11:33:47 -0400

    moved files back one folder

---
+++ routes/authRoute.js
@@ -0,0 +1,17 @@
+const {
+  userRegister,
+  userLogin,
+  userLogout,
+  genImage,
+} = require("../controller/authController");
+
+const { authMiddleware } = require("../middleware/authMiddleware");
+const router = require("express").Router();
+
+router.post("/user-register", userRegister);
+router.post("/user-login", userLogin);
+router.post("/user-logout", authMiddleware, userLogout);
+
+router.post("/gen-image", genImage);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const http = require("http");
+const socketServer = require("./socket");
+
+const databaseConnect = require("./config/database");
+const authRouter = require("./routes/authRoute");
+const chatRouter = require("./routes/chatRoute");
+
+dotenv.config({
+  path: "config/config.env",
+});
+
+const port = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://galactchat.netlify.app",
+      "https://mygchat.netlify.app",
+    ],
+    credentials: true,
+  })
+);
+
+app.set("trust proxy", 1);
+
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+app.use("/api/chat", authRouter);
+app.use("/api/chat", chatRouter);
+
+const server = http.createServer(app);
+
+databaseConnect();
+socketServer(server);
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:5000/`);
+});
